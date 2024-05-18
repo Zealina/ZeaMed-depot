4ac21d23f5f5dcd78d39c98fbf45bc82a3479b36
Zealina Hudson <zealinahudson@gmail.com>
2024-05-18 22:19:04 +0000

    Create Question

---
+++ question/question.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Questions Module contains the question class and all related functions"""
+
+
+class Question:
+    """Class to create questions object"""
+    def __init__():
+        """Initialize the questions class"""
+
+    def to_dict():
+        """Convert instance to dictionary"""
