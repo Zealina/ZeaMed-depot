76ed6bace398f9844625551fd08e83d09c1f4ef5
Zealina Hudson <zealinahudson@gmail.com>
2024-06-26 00:48:11 +0000

    Start Aresh

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Export the storage engine"""
-
-from models.engine.sql_storage import SQLStorage
-
-if 1 + 1 == 2:
-    storage = SQLStorage()
-    storage.reload()
--- parser_main.py
+++
@@ -1,6 +0,0 @@
-from question_parser import parse_question_file
-
-questions = parse_question_file('pharmacology_questions.txt')
-
-for question in questions:
-    print(question.to_dict())
