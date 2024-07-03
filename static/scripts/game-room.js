58a69f14771c6dda9e047f9d9250f87549a8f3d3
Zealina Hudson <zealinahudson@gmail.com>
2024-07-03 20:41:40 +0000

    Done with Add Questions Functtionality

--- static/scripts/game-room.js
+++ static/scripts/game-room.js
@@ -5,8 +5,57 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const sendMessageBtn = document.getElementById('send-message-btn');
     const chatMessages = document.getElementById('chat-messages');
     const playersList = document.getElementById('players');
+    const chatInputForm = document.getElementById('chat-input-form');
+    const formAddQuestion = document.getElementById('form-add-question');
+    const closePopUp = document.getElementById('close-popup');
+    const popupAddQuestion = document.getElementById('popup-add-question');
+    const addQuestionBtn = document.getElementById('add-question-btn');
 
-    sendMessageBtn.addEventListener('click', () => {
+    closePopUp.addEventListener('click', () => popupAddQuestion.setAttribute('aria-hidden', 'true'));
+
+    addQuestionBtn.addEventListener('click', () => popupAddQuestion.setAttribute('aria-hidden', 'false'));
+
+    function validateQuestion(options, correct) {
+        if (!options.includes(',,')) {
+            throw Error('Must be more than One Option');
+        }
+        const optionArray = options.split(',,');
+
+        if (optionArray.includes('')) {
+            throw Error('Empty string cannot be an option');
+        }
+
+        if (isNaN(Number(correct))) {
+            throw Error('Correct Option Index must be a Number');
+        }
+        if (Number(correct) < 0 || Number(correct) > optionArray.length - 1) {
+            throw Error(`Correct Index is must be between 0 and ${optionArray.length - 1} `);
+        }
+    };
+
+    formAddQuestion.addEventListener('submit', e => {
+        try {
+            e.preventDefault();
+            const question = document.getElementById('question-input').value.trim();
+            const options = document.getElementById('options-input').value.trim();
+            const correct = document.getElementById('correct-option-input').value.trim();
+
+            validateQuestion(options, correct);
+            const data = {
+                text: question, 
+                options: options,
+                correct_option_index: Number(correct)
+            }
+            socket.emit('add_question', data);
+            formAddQuestion.reset();
+        }
+        catch (e) {
+            alert(e);
+        }
+    });
+
+    chatInputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
         const message = chatInput.value.trim();
         if (message) {
             socket.emit('new_message', { username: getUsername(), message: message });
